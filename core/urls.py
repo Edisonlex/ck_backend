edbecf53b961ddbd34aa080a112e6b6458b64923
Edisonlex <148796409+Edisonlex@users.noreply.github.com>
2025-08-09 10:08:36 -0500

    zxczx

--- core/urls.py
+++ core/urls.py
@@ -1,33 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
-# from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.urls import path, include
 
-# from apps.cursos.views import CursoViewSet
-# from apps.ubicacion.views import UbicacionViewSet
-# from apps.beneficios.views import BeneficiosViewSet
-
-# ruta = DefaultRouter()
-# ruta.register(r'cursos', CursoViewSet)
-# ruta.register(r'ubicacion', UbicacionViewSet)
-# ruta.register(r'beneficios', BeneficiosViewSet)
-
+# Agrupa todas las URLs de las apps en un archivo urls.py separado
+# o usa un include que apunte a cada app con un prefijo
+api_patterns = [
+    path('', include('apps.inicio.urls')),
+    path('', include('apps.contadores.urls')),
+    path('', include('apps.nosotros.urls')),
+    path('', include('apps.beneficios.urls')),
+    path('', include('apps.galeria.urls')),
+    path('', include('apps.ubicacion.urls')),
+    path('', include('apps.preguntas.urls')),
+    path('', include('apps.contacto.urls')),
+    path('', include('apps.whatsapp.urls')),
+    path('', include('apps.cursos.urls')),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(ruta.urls)),
-    path('api/', include('apps.inicio.urls')),
-    path('api/', include('apps.contadores.urls')),
-    path('api/', include('apps.nosotros.urls')),
-    path('api/', include('apps.beneficios.urls')),
-    path('api/', include('apps.galeria.urls')),
-    path('api/', include('apps.ubicacion.urls')),
-    path('api/', include('apps.preguntas.urls')),
-    path('api/', include('apps.contacto.urls')),
-    path('api/', include('apps.whatsapp.urls')),
-    path('api/', include('apps.cursos.urls')),
-]
-
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('api/', include(api_patterns)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
